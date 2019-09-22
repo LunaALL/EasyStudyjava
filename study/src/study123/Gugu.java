6dbd153f2391b7580e5ba7cb847612b7df99bc71
Woo yong Kim <48975983+LunaALL@users.noreply.github.com>
2019-09-22 13:55:40 +0900

    first

---
+++ study/src/study123/Gugu.java
@@ -0,0 +1,42 @@
+package study123;
+
+public class Gugu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+    /*
+		int sum=0;
+		for(int i =2 ; i<=8; i+=2) {
+			
+			for(int j = 1; j<=i; j++) {
+				sum=i*j;
+				System.out.println(i+" * "+j+ "=" + sum);
+				
+			}
+			
+		}
+		*/
+		
+		
+		
+	int A=0;
+	int B=0;
+	int sum=99;
+	
+	for(A=1; A<=9;A++) {
+		
+		for(B=1; B<=9;B++) {
+			if((A*10+B)+(B*10+A)==sum) {
+				System.out.println("99를 만족하는 A의 값="+A+" B의값="+B);
+			}
+			
+		}
+	}
+	
+		
+		
+		
+	}
+
+}
---
+++ study/src/study123/Mainstudy.java
@@ -0,0 +1,75 @@
+package study123;
+
+public class Mainstudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =1;
+		int sum=1;
+		int count=0;
+		
+	
+       
+		
+		
+		/*
+		
+		while(n<=100) {
+			System.out.println(n);
+			n++;
+		}
+		
+		do {
+			n--;
+			System.out.println(n);
+			
+			
+		}while (n>1);
+		
+		*/
+		
+		
+		
+		/* 
+		
+		while(n<=1000) {
+			if(n%2==0 && n%7==0) {
+				System.out.println("2의 배수이면서 7의 배수 " + n);
+				count++;
+			}
+			n++;
+			
+			
+		}
+		
+		System.out.println("2의 배수이면서 7의 배수 카운트 = " + count);
+		*/
+		
+		
+		
+		
+		/*
+		for (int i =1; i<=10 ; i++) {
+			sum=sum*i;
+			
+			
+		}
+		System.out.println(sum);
+		
+		
+		
+		for(int i=1; i<=10;i++) {
+			sum=5*i;
+			System.out.println("5 * "+ i + "= "+sum);
+			
+		}
+		
+		
+		*/
+		
+		
+
+			
+	}
+
+}
